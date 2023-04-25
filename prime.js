11d280dbea83cb46edeb0253725b58678bec4383
PradeepChakrawarti <pradeepchakrwarti2000@gmail.com>
2023-04-25 21:02:45 +0530

    Help me out

---
+++ prime.js
@@ -0,0 +1,11 @@
+const Prime_Check = (Num) => {
+  let count = 2;
+  for (let i = 1; i < Num; i++) {}
+  if (count > 0) {
+    console.log("Not Prime");
+  } else {
+    console.log("Prime");
+  }
+};
+
+Prime_Check(19);
